bd059081f59560754f900b1ef01a463cbe2a348e
Rodrigo Pimenta Ramos <rodrigopimentaramos@hotmail.com>
2022-08-29 14:49:27 -0300

    Excluindo transações

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -10,12 +10,14 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('R\$${value.toStringAsFixed(2)}'),
+        FittedBox(
+          child: Text('R\$${value.toStringAsFixed(2)}')),
         SizedBox(height: 5,),
         Container(
           height: 60,
           width: 10,
           child: Stack(
+            alignment: Alignment.bottomCenter,
             children: [
               Container(
                 decoration: BoxDecoration(
@@ -24,6 +26,16 @@ class ChartBar extends StatelessWidget {
                     width: 1.0,
                   ),
                   color: Color.fromRGBO(200, 220, 200, 1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
                 ),
               ),
             ],
